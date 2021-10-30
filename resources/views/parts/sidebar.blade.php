2909cb4974ba849e2aeba4135ef0b83e745fe7c8
M. Faris shafwan <farisbos.mfs@gmail.com>
2021-10-30 12:53:52 +0700

    perbaiki sidebar

--- resources/views/parts/sidebar.blade.php
+++ resources/views/parts/sidebar.blade.php
@@ -1,4 +1,14 @@
 <div class="sidebar">
+    <div class="profil p-4 mb-1" style="border-bottom:1px solid white;">
+        <center>
+            <img src="faris.jpg" alt="" class="rounded-circle">
+            <br>
+            <div class="dataprofil" style="color:white;">
+                <h5>Nama</h5>
+                <h6>status</h6>
+            </div>
+        </center>
+    </div>
     <ul>
         <li><a href="#">sidebar</a></li>
         <li><a href="#">sidebar</a></li>
