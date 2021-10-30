9f390f8d5c412fbc04f1af8f68a9b28ea1950d45
M. Faris shafwan <farisbos.mfs@gmail.com>
2021-10-30 13:07:08 +0700

    menambah storage file

--- resources/views/parts/sidebar.blade.php
+++ resources/views/parts/sidebar.blade.php
@@ -1,9 +1,9 @@
 <div class="sidebar">
-    <div class="profil p-4 mb-1" style="border-bottom:1px solid white;">
+    <div class="profil p-4 mb-3" style="border-bottom:1px solid white;">
         <center>
-            <img src="faris.jpg" alt="" class="rounded-circle">
+            <img src="storage/default/faris.jpg" alt="" class="rounded-circle" width="60%">
             <br>
-            <div class="dataprofil" style="color:white;">
+            <div class="dataprofi pt-4" style="color:white;">
                 <h5>Nama</h5>
                 <h6>status</h6>
             </div>
