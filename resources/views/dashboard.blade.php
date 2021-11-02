fa6c7aa75302b27d4db66afa9ad9ce2f5d59e20f
M. Faris shafwan <farisbos.mfs@gmail.com>
2021-11-02 16:57:31 +0700

    faris

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,6 @@
+@extends('parts/body')
+
+@section('body')
+        <center><h1>Halo</h1></center>
+        <p>aaaa</p>
+@endsection
---
+++ resources/views/parts/sidebar.blade.php
@@ -0,0 +1,20 @@
+<div class="sidebar">
+    <div class="profil p-4 mb-3" style="border-bottom:1px solid white;">
+        <center>
+            <img src="storage/default/faris.jpg" alt="" class="rounded-circle" width="60%">
+            <br>
+            <div class="dataprofi pt-4" style="color:white;">
+                <h5>Nama</h5>
+                <h6>status</h6>
+            </div>
+        </center>
+    </div>
+    <ul>
+        <li><a href="#">sidebar</a></li>
+        <li><a href="#">sidebar</a></li>
+        <li><a href="#">sidebar</a></li>
+        <li><a href="#">sidebar</a></li>
+        <li><a href="#">sidebar</a></li>
+        <li><a href="#">sidebar</a></li>
+    </ul>
+</div>
