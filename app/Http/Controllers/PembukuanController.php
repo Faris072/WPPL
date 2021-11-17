113410316ca827b4c8a46da6727ae30dc08248d1
FathanWelldone <fathanwildannyalfalahiakbar@gmail.com>
2021-11-17 18:22:05 +0700

    buat model

---
+++ app/Http/Controllers/PembukuanController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\pembukuan;
+use Illuminate\Http\Request;
+
+class PembukuanController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\pembukuan  $pembukuan
+     * @return \Illuminate\Http\Response
+     */
+    public function show(pembukuan $pembukuan)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\pembukuan  $pembukuan
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(pembukuan $pembukuan)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\pembukuan  $pembukuan
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, pembukuan $pembukuan)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\pembukuan  $pembukuan
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(pembukuan $pembukuan)
+    {
+        //
+    }
+}
