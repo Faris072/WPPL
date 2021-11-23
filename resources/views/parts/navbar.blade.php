985540168005a5076eb80ae9b9bfb166a608c8e7
M. Faris shafwan <farisbos.mfs@gmail.com>
2021-11-23 16:46:16 +0700

    login logout

--- resources/views/parts/navbar.blade.php
+++ resources/views/parts/navbar.blade.php
@@ -1,5 +1,7 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    @auth
     <button id="btnsidebar" style="margin-right:50px;" class="btn btn-outline-light"></button>
+    @endauth
     <a class="navbar-brand" href="#">Navbar</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
         aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
