d858b7e6905a0f18a655ee13c6383a668e04f24e
M. Faris shafwan <farisbos.mfs@gmail.com>
2021-11-25 11:00:24 +0700

    foto

--- resources/views/parts/sidebar.blade.php
+++ resources/views/parts/sidebar.blade.php
@@ -1,7 +1,7 @@
 <div class="sidebar">
     <div class="profil p-4 mb-3" style="border-bottom:1px solid white;">
         <center>
-            <img src="storage/default/faris.jpg" alt="" class="rounded-circle" width="60%">
+            <img src="/storage/default/faris.jpg" alt="" class="rounded-circle" width="60%">
             <br>
             <div class="dataprofi pt-4" style="color:white;">
                 <h5>Nama</h5>
