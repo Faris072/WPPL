840b26ced652e9645effa831668733c3010891cc
farhan2427 <farhanzmuhammad07gmail.com>
2021-12-14 13:28:05 +0700

    edit

--- resources/views/setting.blade.php
+++ resources/views/setting.blade.php
@@ -1,11 +1,18 @@
+
+
 @extends('parts/body')
 
 @section('body')
+<head>
+    <link rel="stylesheet" type="text/css" href="{{asset('css/setting.css')}}">
+</head>
+<body>
+    <h1>Personal info</h1>
     <form action="/profil/{{ Auth::user()->id }}" method="POST" enctype="multipart/form-data">
         @csrf
         @method('PUT')
         <label for="foto">Foto:</label>
-        <input type="file" id="foto" name="foto" class="form-control">
+        <input class="btn btn-primary" type="file" id="foto" name="foto" class="form-control">
         <br>
         <label for="username">Username:</label>
         <input type="text" id="username" name="username" class="form-control" value="{{ $dataProfil->username }}">
@@ -18,4 +25,5 @@
         <br>
         <input type="submit" class="btn btn-success form-control" value="Edit Profil">
     </form>
+
 @endsection
