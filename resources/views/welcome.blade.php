bc4045651553c8145ea6797a0d3b71c231929915
bagas7 <bagas.prakoso3108@gmail.com>
2021-12-14 16:39:55 +0700

    memperbaiki root

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -28,7 +28,8 @@
 
     {{-- navbar --}}
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark" style="position:sticky; z-index:9; top:0;">
-        <a class="navbar-brand" href="#">Navbar</a>
+        <h4 style="color: white;">Pembukuan </h4>
+        
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
             aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
