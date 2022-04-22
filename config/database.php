d99d901aac922a9580d8053f43fc3636e8b37631
Nabila Rindiani Azahra <azahranabila44@gmail.com>
2022-04-23 04:10:21 +0700

    update: Specified key was too long when migrate
    You must change chartset from utf8mb4 to utf8 or configuration boot to make maximum length 191 char or 764b

--- config/database.php
+++ config/database.php
@@ -52,8 +52,12 @@ return [
             'username' => env('DB_USERNAME', 'forge'),
             'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
-            'charset' => 'utf8mb4',
-            'collation' => 'utf8mb4_unicode_ci',
+            /**
+             * https://github.com/laravel/framework/issues/24711#issuecomment-401566425
+             * 
+             */
+            'charset' => 'utf8',
+            'collation' => 'utf8_unicode_ci',
             'prefix' => '',
             'prefix_indexes' => true,
             'strict' => true,
